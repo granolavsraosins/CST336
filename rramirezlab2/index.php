117b7454d10581619aa3705618df310e10bdaf86
raulachu <33336533+granolavsraosins@users.noreply.github.com>
2018-06-26 22:21:32 -0700

    Add files via upload

---
+++ rramirezlab2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style type="text/css">
+            @import url("css/styles.css");
+        </style>
+        <?php 
+        include 'inc/functions.php'; 
+        ?>
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+    <div id="main">
+        
+        <?php 
+        play(); 
+        ?>
+        
+        <form>
+            <input type="submit" value="Spin!"/>
+        </form>
+    </div>    
+    </body>
+</html>
