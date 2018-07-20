af96cd202ad300917467fc361f6abb2908e8274e
granolavsraosins <33336533+granolavsraosins@users.noreply.github.com>
2018-07-19 21:59:21 -0700

    consolidated

---
+++ cst336raulramirez/index.php
@@ -0,0 +1,35 @@
+<html>
+    <body>
+<?php
+    echo "<h1>CST336 Internet Programming : <br>Raul Ramirez </h1>";
+?>
+<h1> Homework </h1>
+<a href="/rramirezhw1/index.php"> Homework 1</a>
+<br />
+<a href="/rramirezhw3/index.php"> Homework 3</a>
+<br /> <br />
+
+<h1> Labs </h1>
+<a href="/rramirezlab1/index.php"> Lab 1 </a>
+<br />
+
+<a href="/rramirezlab2/index.php"> Lab 2</a>
+<br>
+
+<a href="/rramirezlab3/index.php"> Lab 3</a>
+<br>
+
+<a href="/rramirezlab4/index.php"> Lab 4</a>
+<br>
+
+<a href="/rramirezlab5/index.php"> Lab 5</a>
+<br>
+
+<a href="/rramirezlab6/login.php"> Lab 6</a>
+<br>
+
+<a href="/phpmyadmin/index.php"> phpMyAdmin</a>
+<br>
+
+</body>
+</html>
