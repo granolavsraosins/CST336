11f73d25148ad0a4a6fa68be81b45ca97d058f13
granolavsraosins <33336533+granolavsraosins@users.noreply.github.com>
2018-08-01 00:02:31 -0700

    updates

---
+++ cst336raulramirez/rramirezlab9/rramirezlab9/logout.php
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#logoutBtn").click(function() {
+        window.location.href="logout.php";   
+    }
+);
---
+++ cst336raulramirez/rramirezlab9/rramirezlab9/quiz.php
@@ -0,0 +1,40 @@
+<form>
+    <!--Question 1-->
+    What year was CSUMB founded? 
+    <input type="text" name="question1" size="5" /><br />
+    <div id="question1-feedback" class="answer"></div><br />
+    
+    <!--Question 2-->
+    What is the name of CSUMB's mascot?<br />
+    <input type="radio" name="question2" id="q2-1"  value="A"/><label for='q2-1'>Otto <br />
+    <input type="radio" name="question2" id="q2-2"  value="B"/><label for='q2-2'>Albus <br />
+    <input type="radio" name="question2" id="q2-3"  Value="C"/><label for='q2-3'>Monte Rey <br />
+    <div id="question2-feedback" class="answer"></div><br />
+    
+    <!--Question 3-->
+    How many courses are required for the Software Engineering degree?<br />
+    <input type="radio" name="question3" id="q3-1"  value="A"/><label for='q3-1'>13<br />
+    <input type="radio" name="question3" id="q3-2"  value="B"/><label for='q3-2'>11<br />
+    <input type="radio" name="question3" id="q3-3"  Value="C"/><label for='q3-3'>24<br />
+    <div id="question3-feedback" class="answer"></div><br />
+    
+    <!--Question 4-->
+    What does the Academic Mission Statement promise?<br />
+    <input type="radio" name="question3" id="q4-1"  value="A"/><label for='q4-1'>
+        A broad-based, integrated approach to the study of science, technology, and mathematics.<br />
+    <input type="radio" name="question3" id="q4-2"  value="B"/><label for='q4-2'>
+        The application of technology in the solution of a variety of problems in the sciences, mathematics, digital design and computing.<br />
+    <input type="radio" name="question3" id="q4-3"  Value="C"/><label for='q4-3'>
+        a framework for making informed and ethical choices in the information age.<br />
+    <input type="radio" name="question4" id="q4-4"  Value="D"/><label for='q4-4'>All of the above.<br />
+    <div id="question4-feedback" class="answer"></div><br />
+    
+    <input type="submit" value="Submit" />
+    
+    <!--Will display the "loading" or "spinning" animated gif-->
+    <div id="waiting">
+    </div>
+</form>
+
+<!--Will display the quiz score-->
+<div id="scores"></div>
