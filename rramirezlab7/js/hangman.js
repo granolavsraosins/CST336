a206175f17bc4447c568ec3298b994cb6028d7ef
raulachu <33336533+granolavsraosins@users.noreply.github.com>
2018-11-26 13:57:38 -0800

    updated functions

--- rramirezlab7/js/hangman.js
+++ rramirezlab7/js/hangman.js
@@ -1,21 +1,32 @@
 //VARIABLES
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
+                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var words = [{ word: "snake", hint: "It's a reptile" }, 
+             { word: "monkey", hint: "It's a mammal" }, 
+             { word: "beetle", hint: "It's an insect" }];
+
 var selectedWord = "";
 var selectedHint = "";
-var board = [];
+var board = "";
 var remainingGuesses = 6;
-var hint = false;
-var words = [{word:"snake", hint: "It's a reptile"},
-             {word:"monkey", hint: "It's a mammal"},
-             {word:"beetle", hint: "It's an insect"}];
-//creating an array
-var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T',
-                'U','V','W','X','Y','Z'];
-//LISTENERS
+
 window.onload = startGame();
+
+$(".replayBtn").on('click', function() {
+    location.reload();
+});
+
+$("#letters").on("click", ".letter", function(){
+    checkLetter($(this).attr("id"));
+    disableButton($(this));
+});
 
 //FUNCTIONS
 function startGame() {
     pickWord();
+    createLetters();
     initBoard();
     updateBoard();
 }
@@ -25,19 +36,19 @@ function initBoard() {
         board.push("_");
     }
 }
+//picks a random word from the array and sets it to upper case into a new variable
+//the hint is the same as the random word
 function pickWord() {
     var randomInt = Math.floor(Math.random() * words.length);
     selectedWord = words[randomInt].word.toUpperCase();
     selectedHint = words[randomInt].hint;
 }
+//puts the amount of underscores on the board according to the word
 function updateBoard() {
     $("#word").empty();
-    
     for (var i=0; i < board.length; i++){
-        $("#word").append(board[i] + " ");
+        document.getElementById("word").innerHTML += letter + " ";
     }
-    $("#word").append("<br/>");
-    $("#word").append("<span class='hint'>Hint: " + selectedHint + "</span>");
 }
 //Creates the letters inside the letters div
 function createLetters(){
@@ -45,41 +56,34 @@ function createLetters(){
         $("#letters").append("<button class='letter' id='" + letter + "'>" + letter + "</button>");
     }
 }
-//Update the current word then calls for a board update
-function updateWord(positions,letter){
-    for (var pos of positions){
-        board[pos] = letter;
+//populates the board according to the letter chosen and places the hint on the page
+function updateBoard() {
+    $("#word").empty();
+    for (var letter of board) {
+        $("#word").append(letter);
+        $("#word").append(' ');
     }
-    updateBoard();
+    $("#word").append("<br />");
 }
-
 //Checks to see if the selected letter exists in the selected Word
-function checkLetter(letter){
+function checkLetter(letter) {
     var positions = new Array();
     
-    //Put all the positions the letter exists in the array
-    for(var i=0; i < selectedWord.length; i++){
-        console.log(selectedWord)
-        if (letter == selectedWord[i]){
+    for (var i = 0; i < selectedWord.length; i++) {
+        if (letter == selectedWord[i]) {
             positions.push(i);
         }
     }
-    if(positions.length > 0){
-        updateWord(positions,letter);
-        
-        //check to see if this is winning guess
-        if(!board.includes('_')){
-            endGame(true);
-        }
-    } else{
+    if (positions.length > 0) {
+        updateWord(positions, letter);
+    } else {
         remainingGuesses -= 1;
         updateMan();
-    }
-    if(remainingGuesses <= 0){
+        if (remainingGuesses <= 0) {
         endGame(false);
+        }
     }
 }
-
 //Calculates and updates the image for our stick man
 function updateMan(){
     $("#hangImg").attr("src","img/stick_" + (6 - remainingGuesses) + ".png");
@@ -100,7 +104,7 @@ function disableButton(btn){
 }
 
 $(".letter").click(function() {
-    console.log($(this).attr("id"));
+    checkLetter($(this).attr("id"));
     disableButton($(this));
 });
 
@@ -109,6 +113,34 @@ $("#letterBtn").click(function(){
     console.log("You pressed the button and it has the value: " + boxVal);
 });
 
-$(".replayBtn").on("click",function(){
-    location.reload();
+$(".hint").on("click", function() {
+    $(".hint").text("Hint: " + selectedHint);
 });
+
+function createLetters() {
+    for(var letter of alphabet) {
+        let letterInput = '"' + letter + '"';
+        $("#letters").append("<button class='btn btn-success letter' id='" + letter + "'>" + letter + "</button>");
+    }
+}
+//will accept correct letter for the guessword
+function replaceAt(str, index, value) {
+    return str.substr(0, index) + value + str.substr(index + value.length);
+}
+//places underscores on the page according to what the word is.
+function initBoard() {
+    for (var letter in selectedWord) {
+        board += '_';
+    }
+}
+//put the letters on the board and replaces the underscores
+function updateWord(positions, letter) {
+    for (var pos of positions) {
+        board = replaceAt(board, pos, letter)
+    }
+    updateBoard(board);
+    if (!board.includes('_')) {
+        endGame(true);
+    }
+}
+
